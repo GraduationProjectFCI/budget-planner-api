6f798c8ef5a2923b5d8154fb45de7714f388ec0f
Momen Khaled <momenk208@gmail.com>
2023-05-25 23:40:42 +0300

    ...

--- controllers/app.js
+++ controllers/app.js
@@ -1306,14 +1306,12 @@ const deleteSheet = (req, res) => {
           } else {
             Sheets.findByIdAndDelete(sheet_id)
               .then(async () => {
-                await Expenses.deleteMany({ sheet_id: sheet_id }).then(
-                  async () => {
-                    await SheetValue(sheet_id);
-                    await UpdateUserData(authData.userId, sheet_id);
-                    await CalcLimitValue(authData.userId, sheet_id);
-                  }
-                );
-                await Do_Statistics(authData.userId);
+                Expenses.deleteMany({ sheet_id: sheet_id }).then(async () => {
+                  await SheetValue(sheet_id);
+                  await UpdateUserData(authData.userId, sheet_id);
+                  await CalcLimitValue(authData.userId, sheet_id);
+                  await Do_Statistics(authData.userId);
+                });
 
                 res.status(200).json({
                   msg: "Sheet Deleted Successfully",
