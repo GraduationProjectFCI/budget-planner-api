692b85edfbaf7952ad48f8544efd42e0441e9780
Momen Khaled <momenk208@gmail.com>
2023-05-25 23:11:15 +0300

    fix states calculations

--- controllers/statistics.js
+++ controllers/statistics.js
@@ -2,56 +2,59 @@ const Lebels = require("../models/LabelSchema");
 const Expenses = require("../models/expenses");
 const UserData = require("../models/user_data_modal");
 const statistics = require("../models/statesSchema");
+const Sheets = require("../models/sheetSchema");
 
 const Do_Statistics = async (user_id) => {
   //delete the old statistics if found
-  await statistics.deleteMany({ user_id: user_id });
+  statistics.deleteMany({ user_id: user_id }).then(async () => {
+    //User Total Budget
+    const user_data = await UserData.findOne({ user_id: user_id });
 
-  //User Total Budget
-  const user_data = await UserData.findOne({ user_id: user_id });
+    // get user labels
 
-  // get user labels
-  const labels = await Lebels.find({ user_id: user_id });
+    const allExportSheetExpenses = [];
 
-  var labelsList = [];
-
-  labels.forEach((label) => {
-    labelsList.push(label.label);
-  });
-
-  //get user expenses
-  labelsList.forEach(async (label) => {
-    var expensesSum = 0;
-    var labelPercentageTotal = 0;
-    var labelPercentageSpent = 0;
-    const expenses = await Expenses.find({
-      user_id,
-      label,
+    //get user sheets with the type export
+    const sheets = await Sheets.find({
+      user_id: user_id,
+      sheet_type: "export",
     });
 
-    if (expenses.length > 0) {
-      expenses.forEach((expense) => {
-        expensesSum += expense.value;
+    //get all expenses for each sheet
+    sheets.forEach(async (sheet) => {
+      const expenses = await Expenses.find({ sheet_id: sheet._id });
+      if (expenses.length > 0) {
+        expenses.forEach(async (expense) => {
+          allExportSheetExpenses.push(expense);
+        });
+      }
+    });
+
+    const labels = await Lebels.find({ user_id: user_id });
+
+    if (labels.length > 0) {
+      labels.forEach(async (label) => {
+        let labelSum = 0;
+        allExportSheetExpenses.map((expense) => {
+          if (expense.label === label.label) {
+            labelSum += expense.value;
+          }
+        });
+
+        const labelPercentageTotal = (labelSum / user_data.total) * 100;
+        const labelPercentageSpent = (labelSum / user_data.spent) * 100;
+
+        const newStatistics = new statistics({
+          user_id: user_id,
+          label: label.label,
+          expensesSum: labelSum,
+          labelPercentageTotal: labelPercentageTotal,
+          labelPercentageSpent: labelPercentageSpent,
+        });
+
+        await newStatistics.save();
       });
-
-      //get the percentage of each label from the total budget
-
-      //trim the number to only 2 numbers after the dot
-      labelPercentageTotal = ((expensesSum / user_data.total) * 100).toFixed(2);
-
-      //get the percentage of each label from the spent budget
-      labelPercentageSpent = ((expensesSum / user_data.spent) * 100).toFixed(2);
     }
-
-    // set the states in the states collection
-    const state = new statistics({
-      user_id,
-      label,
-      expensesSum,
-      labelPercentageTotal,
-      labelPercentageSpent,
-    });
-    await state.save();
   });
 };
 
